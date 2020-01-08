8ac0ce141fe4d08d8e93ff902b8903c82b93f95b
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-01-08 22:44:22 +0000

    Cleanup (#11)
    * Apply eslint & prettier
    
    * Fix vulnerabilities
    
    * Move ping into actual bot instead of test
    
    * complete testing pattern template

--- test/BotTests.ts
+++ test/BotTests.ts
@@ -3,31 +3,44 @@ import * as TelegrafTest from 'telegraf-test';
 import { Settings } from '../src/Settings';
 import AuthenticateFromInvitation from '../src/middleware/AuthenticateFromInvitation';
 import ForecastsBot from '../src/ForecastsBot';
+import { MockOperations } from './MockOperations';
+import { validInvitationId } from './fixtures/Fixtures';
+import { basicPlayer } from './fixtures/PlayerFixtures';
 
 const port = 3000;
 const secretPath = 'secret-path';
 
 const settings: Settings = {
     tokenId: 'ABCD:1234567890',
-    dataOperations: null
+    dataOperations: new MockOperations()
 };
 
 const mw = AuthenticateFromInvitation(settings);
 
-const bot = ForecastsBot(settings);
-
-bot.use(mw);
+const bot = ForecastsBot(settings, [mw]);
 
 bot.startWebhook(`/${secretPath}`, null, port);
 
-const test = new TelegrafTest({
+const client = new TelegrafTest({
     url: `http://127.0.0.1:${port}/${secretPath}`
 });
 
 describe('forecasts-bot', () => {
-    it('/ping', async () => {
-        const r = await test.sendMessageWithText('/ping');
+    it('is alive', async () => {
+        const r = await client.sendMessageWithText('/ping');
         expect(r.data.text).to.be.a('string');
         expect(r.data.text).to.contain('Pong!');
+    });
+    describe('AuthenticateFromInvitation middleware', () => {
+        it('should set the player context from invitation ID', async () => {
+            const result = await client.sendMessageWithText(
+                `/start ${validInvitationId}`
+            );
+
+            expect(result.data.text).to.be.a('string');
+            expect(result.data.text).to.be(
+                `Evening, ${basicPlayer.displayName}.`
+            );
+        });
     });
 });
---
+++ test/fixtures/Fixtures.ts
@@ -0,0 +1,1 @@
+export const validInvitationId: string = '08ce53d1-0688-46c0-80c4-d6fa1dc2dc86';
