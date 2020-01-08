f83d5969da1c6d5930b2a07761d62badca349b76
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-01-08 13:30:14 +0000

    Cleanup (#10)
    * Fix vulnerabilities
    
    * Move ping into actual bot instead of test

--- src/ForecastsBot.ts
+++ src/ForecastsBot.ts
@@ -3,5 +3,11 @@ import { ForecastsContext } from './ForecastsContext';
 import { Settings } from './Settings';
 
 export default (settings: Settings): Telegraf<ForecastsContext> => {
-    return new Telegraf(settings.tokenId);
+    const bot = new Telegraf(settings.tokenId);
+
+    bot.hears(/ping/i, (ctx: ForecastsContext) => {
+        ctx.reply('Pong!');
+    });
+
+    return bot;
 };
