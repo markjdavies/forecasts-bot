2c1a6c66201c2580c711e4f0c8ab450b4f21d8db
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-01-08 22:40:12 +0000

    complete testing pattern template

---
+++ test/fixtures/PlayerFixtures.ts
@@ -0,0 +1,8 @@
+import { Player } from '../../src/dataModel/Player';
+
+export const basicPlayer: Player = {
+    playerId: 1,
+    userName: 'player1',
+    displayName: 'Player 1',
+    team: 'Player One FC'
+};
