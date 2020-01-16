db92158da66c54c659abe4948838c912c4d27a98
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-01-16 13:27:19 +0000

    Added missing index file

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * from './src/ForecastsBot';
+export * from './src/dal/Mssql/MssqlDataOperations';
+export * from './src/middleware/AuthenticateFromInvitation';
+export * from './src/Settings';
