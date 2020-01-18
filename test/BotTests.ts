9bfd9734dca373b6eb667d8d6f3c8ecad6867993
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-01-18 21:05:11 +0000

    Fix command tests (#27)

--- test/BotTests.ts
+++ test/BotTests.ts
@@ -15,6 +15,7 @@ const settings: Settings = {
     dataOperations: new MockOperations()
 };
 
+
 const mw = authenticateFromInvitation(settings);
 
 const bot = forecastsBot(settings, [mw]);
@@ -25,31 +26,41 @@ const client = new TelegrafTest({
     url: `http://127.0.0.1:${port}/${secretPath}`
 });
 
+const sendCommand = async (text: string) => {
+    return await client.sendMessageWithText(text, {
+        entities: [{
+            offset: 0,
+            length: text.length,
+            type: 'bot_command'
+        }]
+    });
+};
+
 test('it is alive', async t => {
-    const r = await client.sendMessageWithText('/good');
+    const r = await sendCommand('/good');
     t.is(r.data.text, 'Good, good, good!');
 });
 
 test('should acknowledge an anonymous user', async t => {
-    const result = await client.sendMessageWithText(`/start`);
+    const result = await sendCommand(`/start`);
     t.is(result.data.text, `Evening, chief.`);
 });
 
 test('should disregard an unrecognised invitation', async t => {
-    const result = await client.sendMessageWithText(
+    const result = await sendCommand(
         `/start 49b9f2b7-4c79-4523-b0f9-6ba22b5fca8d`
     );
     t.is(result.data.text, `Evening, chief.`);
 });
 
 test('should recognise an invited player', async t => {
-    const result = await client.sendMessageWithText(
+    const result = await sendCommand(
         `/start ${validInvitationId}`
     );
     t.is(result.data.text, `Evening, ${basicPlayer.displayName}.`);
 });
 
 test('should provide next round date on nextFixture command', async t => {
-    const result = await client.sendMessageWithText('/nextFixture');
+    const result = await sendCommand('/nextfixture');
     t.is(result.data.text, 'Next matches: Tue 14th Jan (Cup Quarter Finals)');
 });
