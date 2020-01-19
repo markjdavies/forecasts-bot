51246ae27ecf64f53b4712541314948c72cf6319
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-01-19 22:22:52 +0000

    Fixture and mock operation

--- test/MockOperations.ts
+++ test/MockOperations.ts
@@ -1,6 +1,6 @@
 import { DataOperations } from '../src/dal/DataOperations';
 import { Player } from '../src/dataModel/Player';
-import { validInvitationId, roundDate } from './fixtures/Fixtures';
+import { validInvitationId, roundDate, playerPhoneNumber } from './fixtures/Fixtures';
 import { basicPlayer } from './fixtures/PlayerFixtures';
 import { RoundDate } from '~src/dataModel/RoundDate';
 
@@ -8,10 +8,21 @@ export class MockOperations implements DataOperations {
     public async GetNextFixture(): Promise<RoundDate> {
         return roundDate;
     }
+
     public async GetPlayerFromInvitationId(
         invitationGuid: string
     ): Promise<Player> {
         if (invitationGuid === validInvitationId) {
+            return basicPlayer;
+        } else {
+            return null;
+        }
+    }
+
+    public async GetPlayerFromMobileNumber(
+        mobileNumber: string
+    ): Promise<Player> {
+        if (mobileNumber === playerPhoneNumber) {
             return basicPlayer;
         } else {
             return null;
--- test/fixtures/Fixtures.ts
+++ test/fixtures/Fixtures.ts
@@ -2,6 +2,8 @@ import { RoundDate } from '~src/dataModel/RoundDate';
 
 export const validInvitationId: string = '08ce53d1-0688-46c0-80c4-d6fa1dc2dc86';
 
+export const playerPhoneNumber: string = '0123456456';
+
 export const roundDate: RoundDate = {
     date: new Date('14 January 2020'),
     roundName: 'Cup Quarter Finals'
