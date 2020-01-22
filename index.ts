78adee88fda8b6f9c3869642dc2d4879be622875
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-01-22 19:09:20 +0000

    Missed export in index

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from './src/forecastsBot';
 export * from './src/dal/Mssql/MssqlDataOperations';
 export * from './src/middleware/authenticateFromInvitation';
+export * from './src/middleware/authenticateFromChatId';
 export * from './src/Settings';
