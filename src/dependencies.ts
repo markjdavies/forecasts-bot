877a514112fef1e730d1850eab403fe06dd01d00
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-08-14 10:09:35 +0100

    Update dependencies.ts

--- src/dependencies.ts
+++ src/dependencies.ts
@@ -1,7 +1,7 @@
 import { DbConnectionConfigModel } from './dal/Mssql/DbConnectionConfig';
 import { MssqlDataOperations } from './dal/Mssql/MssqlDataOperations';
 
-export const dbConfig = DbConnectionConfigModel.parse(process.env.db);
+export const dbConfig = DbConnectionConfigModel.parse(JSON.parse(process.env.db));
 export const dataOperations = new MssqlDataOperations(dbConfig);
 
 export const settings = {
