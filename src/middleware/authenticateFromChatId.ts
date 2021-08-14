ff59a721fc2f9e8bea4705b2929f8cef2b355a1b
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-08-14 20:14:09 +0100

    add some trace logging (#276)

--- src/middleware/authenticateFromChatId.ts
+++ src/middleware/authenticateFromChatId.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { ForecastsContext } from '~src/ForecastsContext';
 import { Settings } from '~src/Settings';
 
@@ -9,8 +10,12 @@ export const authenticateFromChatId = (
     const mw = async (ctx: ForecastsContext, next: Function): Promise<void> => {
         const chatId = ctx?.message?.chat?.id;
         const player = await operations.GetPlayerFromChatId(chatId);
+
         if (player) {
+            console.info(`Recognised ${player.displayName}`);
             ctx.player = player;
+        } else {
+            console.info('Did not recognise player from chatId');
         }
         await next();
     };
