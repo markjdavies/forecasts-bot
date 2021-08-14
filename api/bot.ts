75b5701b1ae4993e2b3611854a2b5e24eb4ff2e4
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-08-14 19:53:14 +0100

    Return proper response from handler - bot as m/w

--- api/bot.ts
+++ api/bot.ts
@@ -1,7 +1,13 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { chain } from '@amaurym/now-middleware';
 import { settings } from '../src/dependencies';
 import { authenticateFromChatId } from '../src/middleware/authenticateFromChatId';
 import { authenticateFromInvitation } from '../src/middleware/authenticateFromInvitation';
 import { forecastsBot } from '../src/forecastsBot';
+
+const handler = async (_req: VercelRequest, res: VercelResponse) => {
+    res.status(200).send('Bot request handled.');
+};
 
 const mw = [
     authenticateFromInvitation(settings),
@@ -10,4 +16,4 @@ const mw = [
 
 const bot = forecastsBot(settings, mw);
 
-export default bot;
+export default chain(bot)(handler);
