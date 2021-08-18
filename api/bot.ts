334a92edd02d49a804544c76c94889561828a859
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-08-18 19:54:36 +0100

    put bot inside request handler (#280)

--- api/bot.ts
+++ api/bot.ts
@@ -1,14 +1,15 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import { chain } from '@amaurym/now-middleware';
+// import { chain } from '@amaurym/now-middleware';
 import { settings } from '../src/dependencies';
 import { authenticateFromChatId } from '../src/middleware/authenticateFromChatId';
 import { authenticateFromInvitation } from '../src/middleware/authenticateFromInvitation';
 import { forecastsBot } from '../src/forecastsBot';
 
-async function handler(
+export default async function handler(
     _req: VercelRequest,
     res: VercelResponse
 ): Promise<void> {
+    forecastsBot(settings, mw);
     res.send('Bot request handled.');
 }
 
@@ -16,7 +17,3 @@ const mw = [
     authenticateFromInvitation(settings),
     authenticateFromChatId(settings),
 ];
-
-const bot = forecastsBot(settings, mw);
-
-export default chain(bot)(handler);
