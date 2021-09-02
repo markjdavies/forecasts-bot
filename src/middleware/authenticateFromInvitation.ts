96f02f61816f5805255f631648c3825e31f32fe5
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-09-02 20:17:19 +0100

    Fix invitation auth mw

--- src/middleware/authenticateFromInvitation.ts
+++ src/middleware/authenticateFromInvitation.ts
@@ -1,12 +1,21 @@
 import { Settings } from '~src/Settings';
 import { ForecastsContext } from '~src/ForecastsContext';
+import { VercelResponse } from '@vercel/node';
 
 export const authenticateFromInvitation = (
     settings: Settings
-): ((ctx: ForecastsContext, next: Function) => Promise<void>) => {
+): ((
+    ctx: ForecastsContext,
+    res: VercelResponse,
+    next: Function
+) => Promise<void>) => {
     const operations = settings.dataOperations;
 
-    const mw = async (ctx: ForecastsContext, next: Function): Promise<void> => {
+    const mw = async (
+        ctx: ForecastsContext,
+        _res: VercelResponse,
+        next: Function
+    ): Promise<void> => {
         if (!ctx.player) {
             if (ctx.body?.message?.text.indexOf('/start ') >= 0) {
                 const invitationGuid = ctx.body?.message.text.replace(
